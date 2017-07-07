842ea6de7dde6040b1f4463a6c393fdd522aaa0a
wangchen <ccwz07@163.com>
2017-07-07 13:18:01 +0800

    Initial commit

---
+++ app/src/main/java/com/drwang/views/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.drwang.views.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.drwang.views.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
