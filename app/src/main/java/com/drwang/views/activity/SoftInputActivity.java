e4607619a644b18f43c6c1f9d35a0cc769c8ccf3
wangchen <ccwz07@163.com>
2017-07-25 17:47:08 +0800

    reveal reflect

--- app/src/main/java/com/drwang/views/activity/SoftInputActivity.java
+++ app/src/main/java/com/drwang/views/activity/SoftInputActivity.java
@@ -1,9 +1,14 @@
 package com.drwang.views.activity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
@@ -15,6 +20,11 @@ import com.drwang.views.base.BasicActivity;
 import com.drwang.views.util.InputMethodManagerUtil;
 import com.drwang.views.view.ScanView;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -23,12 +33,12 @@ public class SoftInputActivity extends Activity {
     @BindView(R.id.rl_root)
     View rl_root;
     private ScanView scan;
+    private static final String TAG = "SoftInputActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // 更改状态栏颜色
-
         setContentView(R.layout.activity_soft_input);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
@@ -45,6 +55,35 @@ public class SoftInputActivity extends Activity {
                 Toast.makeText(SoftInputActivity.this, "zerozero", Toast.LENGTH_SHORT).show();
             }
         });
+        try {
+            Class aClass = ScanView.class;
+            Method method2 = aClass.getDeclaredMethod("getRandomColor");
+            Method method = aClass.getDeclaredMethod("setTimeStart", int.class);
+            method.setAccessible(true);
+            method.invoke(scan, 100);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertTheme).setCancelable(true).setTitle("xixi").setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).setNeutralButton("haha", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).setNegativeButton("22", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(Color.RED);
 //        int size = (int) (getResources().getDisplayMetrics().density * 5);
 //        ButterKnife.bind(this);
 //        rl_root.setOnTouchListener(new View.OnTouchListener() {
