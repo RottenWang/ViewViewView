0f8792149c015b07b7bed69666bcb3343b569f8c
Dr.Wang <80250706@qq.com>
2017-08-09 20:54:19 +0800

    小米相机 发送按钮图标设计

---
+++ app/src/main/java/com/drwang/views/view/CircleBgImageView.java
@@ -0,0 +1,77 @@
+package com.drwang.views.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.CornerPathEffect;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2017/8/9.
+ */
+
+public class CircleBgImageView extends View implements View.OnClickListener {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    float density;
+    private Path path;
+    private CornerPathEffect cornerPathEffect;
+
+    public CircleBgImageView(Context context) {
+        this(context, null);
+    }
+
+    public CircleBgImageView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CircleBgImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        setBackgroundColor(Color.TRANSPARENT);
+        density = getResources().getDisplayMetrics().density;
+        paint.setColor(Color.WHITE);
+        path = new Path();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setFilterBitmap(true);
+        cornerPathEffect = new CornerPathEffect(density);
+        setOnClickListener(this);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int width = getWidth();
+        int height = getHeight();
+        paint.setStrokeWidth(density / 2);
+        canvas.drawCircle(width / 2, height / 2, width / 2 - (density * 2), paint);
+        path.reset();
+        path.moveTo(width / 3 + 3 * density, height / 3);
+        path.lineTo(width / 3, height / 3);
+        path.lineTo(width / 3, (height / 3) * 2);
+        path.lineTo((width / 3) * 2, (height / 3) * 2);
+        path.lineTo((width / 3) * 2, (height / 3) * 2 - 3 * density);
+        path.moveTo(width / 3 + 5 * density, height / 3);
+        path.lineTo((width / 3) * 2, height / 3);
+        path.lineTo((width / 3) * 2, (height / 3) * 2 - 5 * density);
+        path.moveTo((width / 3) * 2, height / 3);
+        path.lineTo(width / 2, height / 2);
+        paint.setPathEffect(cornerPathEffect);
+        paint.setStrokeWidth(density);
+        canvas.drawPath(path, paint);
+        paint.setPathEffect(null);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getContext(), "分享", Toast.LENGTH_SHORT).show();
+    }
+}
