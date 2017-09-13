97853f687ced8fb3c40b48c327c84b75b884b277
Dr.Wang <80250706@qq.com>
2017-09-13 15:00:38 +0800

    用java 写一遍loader

--- app/src/main/java/com/drwang/views/activity/SplashActivity.java
+++ app/src/main/java/com/drwang/views/activity/SplashActivity.java
@@ -1,11 +1,25 @@
 package com.drwang.views.activity;
 
+import android.Manifest;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 
 import com.drwang.views.R;
+import com.drwang.views.bean.ImageEntityBean;
+
+import java.util.ArrayList;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -18,5 +32,79 @@ public class SplashActivity extends AppCompatActivity {
             finish();
             overridePendingTransition(R.anim.alpha_in, R.anim.alpha_out);
         }, 2000);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//                requestPermissions(new String[]{(Manifest.permission.WRITE_EXTERNAL_STORAGE)}, 100);
+//            } else {
+//                getSupportLoaderManager().initLoader(0, null, imageLoader);
+//            }
+//        } else {
+//            getSupportLoaderManager().initLoader(0, null, imageLoader);
+//
+//        }
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getSupportLoaderManager().initLoader(0, null, imageLoader);
+            }
+        }
+    }
+
+    LoaderManager.LoaderCallbacks<Cursor> imageLoader = new LoaderManager.LoaderCallbacks<Cursor>() {
+
+        private String[] IMAGE_PROJECTION = {
+                MediaStore.Images.Media.DATA,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.DATE_ADDED,
+                MediaStore.Images.Media.WIDTH,
+                MediaStore.Images.Media.HEIGHT,
+                MediaStore.Images.Media._ID
+        };
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            CursorLoader cursorLoader = new CursorLoader(getApplicationContext(),
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_PROJECTION, null, null, IMAGE_PROJECTION[2] + " DESC"
+            );
+            return cursorLoader;
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+            if (data != null) {
+                if (data.moveToFirst()) {
+                    do {
+                        String path = data.getString(data.getColumnIndexOrThrow(IMAGE_PROJECTION[0]));
+                        String name = data.getString(data.getColumnIndexOrThrow(IMAGE_PROJECTION[1]));
+                        long dateTime = data.getLong(data.getColumnIndexOrThrow(IMAGE_PROJECTION[2]));
+                        int width = data.getInt(data.getColumnIndexOrThrow(IMAGE_PROJECTION[3]));
+                        int height = data.getInt(data.getColumnIndexOrThrow(IMAGE_PROJECTION[4]));
+                        if (TextUtils.isEmpty(path) || TextUtils.isEmpty(name)) {
+                            continue;
+                        }
+                        if (width <= 0 || height <= 0) {
+                            continue;
+                        }
+
+
+                    } while (data.moveToNext());
+                }
+
+
+            }
+
+        }
+
+    };
 }
