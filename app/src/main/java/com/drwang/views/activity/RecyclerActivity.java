fb9e6482c735965d90bbbba635b1cf692052472c
RottenWang <80250706@qq.com>
2018-01-11 16:49:53 +0800

    commit

--- app/src/main/java/com/drwang/views/activity/RecyclerActivity.java
+++ app/src/main/java/com/drwang/views/activity/RecyclerActivity.java
@@ -1,9 +1,17 @@
 package com.drwang.views.activity;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.PointF;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSmoothScroller;
 import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.TextView;
 
 import com.drwang.views.R;
@@ -30,6 +38,7 @@ public class RecyclerActivity extends BasicActivity {
     ExampleAdapter exampleAdapter;
     List<String> mList;
     LinearLayoutManager layoutManager;
+    GridLayoutManager gridLayoutManager;
 
 
     @Override
@@ -51,11 +60,15 @@ public class RecyclerActivity extends BasicActivity {
         textView.setText("this is header");
         TextView textView2 = (TextView) footerView.findViewById(R.id.tv_test);
         textView2.setText("this is footer");
-//        exampleAdapter.setHeaderView(headerView);
+        exampleAdapter.setHeaderView(headerView);
 //        exampleAdapter.setFooterView(footerView);
         if (layoutManager == null)
             layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(layoutManager);
+        if (gridLayoutManager == null) {
+            gridLayoutManager = new MineGridLayoutManager(this, 3);
+        }
+        gridLayoutManager.setSpanSizeLookup(new SpanSize());
+        recyclerView.setLayoutManager(gridLayoutManager);
         recyclerView.setAdapter(exampleAdapter);
         nll_root.addToShowViewList(tv_title2);
         nll_root.addToHideViewList(tv_title3);
@@ -76,5 +89,74 @@ public class RecyclerActivity extends BasicActivity {
 //        exampleAdapter.notifyItemRangeChanged(0, mList.size()-1);
         recyclerView.scrollToPosition(mList.size() - 1);
 //        recyclerView.smoothScrollToPosition(mList.size() - 1);
+    }
+
+    private static class MineGridLayoutManager extends GridLayoutManager {
+
+        private float MILLISECONDS_PER_INCH = 0.03f;
+        private Context mContext;
+
+        public MineGridLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+            super(context, attrs, defStyleAttr, defStyleRes);
+            mContext = context;
+        }
+
+        public MineGridLayoutManager(Context context, int spanCount) {
+            super(context, spanCount);
+            mContext = context;
+        }
+
+        public MineGridLayoutManager(Context context, int spanCount, int orientation, boolean reverseLayout) {
+            super(context, spanCount, orientation, reverseLayout);
+            mContext = context;
+        }
+
+        @Override
+        public void smoothScrollToPosition(RecyclerView recyclerView, RecyclerView.State state, int position) {
+//            super.smoothScrollToPosition(recyclerView, state, position);
+            LinearSmoothScroller linearSmoothScroller =
+                    new LinearSmoothScroller(recyclerView.getContext()) {
+                        @Override
+                        public PointF computeScrollVectorForPosition(int targetPosition) {
+                            return MineGridLayoutManager.this
+                                    .computeScrollVectorForPosition(targetPosition);
+                        }
+
+                        //This returns the milliseconds it takes to
+                        //scroll one pixel.
+                        @Override
+                        protected float calculateSpeedPerPixel
+                        (DisplayMetrics displayMetrics) {
+                            return MILLISECONDS_PER_INCH / displayMetrics.density;
+                            //返回滑动一个pixel需要多少毫秒
+                        }
+
+                    };
+            linearSmoothScroller.setTargetPosition(position);
+            startSmoothScroll(linearSmoothScroller);
+        }
+
+        public void setSpeedSlow() {
+            //自己在这里用density去乘，希望不同分辨率设备上滑动速度相同
+            //0.3f是自己估摸的一个值，可以根据不同需求自己修改
+            MILLISECONDS_PER_INCH = mContext.getResources().getDisplayMetrics().density * 0.3f;
+        }
+
+        public void setSpeedFast() {
+            MILLISECONDS_PER_INCH = mContext.getResources().getDisplayMetrics().density * 0.03f;
+        }
+
+    }
+
+    public class SpanSize extends GridLayoutManager.SpanSizeLookup {
+
+        @Override
+        public int getSpanSize(int position) {
+            if (position == 0) {
+                return 3;
+            } else {
+                return 1;
+            }
+        }
     }
 }
