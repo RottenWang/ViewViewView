db79f45c0c4e74cbc32f7d8bb02180be3e4414f6
RottenWang <80250706@qq.com>
2018-02-24 15:20:22 +0800

    !!!!

--- app/src/main/java/com/drwang/views/view/ArtTextView.java
+++ app/src/main/java/com/drwang/views/view/ArtTextView.java
@@ -10,6 +10,7 @@ import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -48,17 +49,25 @@ public class ArtTextView extends View {
 
     }
 
-
+    float controlX = 0;
+    float controlY = 0;
+    boolean isFirst = true;
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         path.reset();
         int width = getWidth();
         int height = getHeight();
         int centerX = width / 2;
         int centerY = height / 2;
+        if (isFirst){
+            controlY = -centerY / 2;
+            controlX = centerX / 2 - 20;
+            isFirst = false;
+        }
         path.moveTo(20, centerY);
-        path.rQuadTo(centerX / 2 - 20, -centerY / 2, centerX - 20, 0);
+        path.rQuadTo(controlX, controlY, centerX - 20, 0);
         if (mLinearGradient == null) {
             mLinearGradient = new LinearGradient(0, 0, centerX, 0, colors, null, Shader.TileMode.CLAMP);
         }
@@ -68,5 +77,41 @@ public class ArtTextView extends View {
         canvas.drawTextOnPath(text, path, 0, 0, textPaint);
         textPaint.setShader(null);
         canvas.restore();
+    }
+
+    float startX = 0;
+    float startY = 0;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startX = event.getX();
+                startY = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (startX == 0 || startY == 0) {
+                    startX = event.getX();
+                    startY = event.getY();
+                    break;
+                }
+                float moveX = event.getX();
+                float moveY = event.getY();
+                float distanceX = moveX - startX;
+                float distanceY = moveY - startY;
+                startX = moveX;
+                startY = moveY;
+                controlX += distanceX;
+                controlY += distanceY;
+                invalidate();
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                startX = 0;
+                startY = 0;
+                break;
+        }
+        return true;
+
     }
 }
