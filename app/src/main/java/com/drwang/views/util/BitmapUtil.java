98bef75d85351214ff020022c2e61c9523bb32c4
Hello World <80250706@qq.com>
2018-04-10 21:07:05 +0800

    Update BitmapUtil.java

--- app/src/main/java/com/drwang/views/util/BitmapUtil.java
+++ app/src/main/java/com/drwang/views/util/BitmapUtil.java
@@ -115,5 +115,67 @@ public class BitmapUtil {
         bitmap = Bitmap.createBitmap(source, 0, 0, sourceWidth, sourceHeight, matrix, false);
         source.recycle();
         return bitmap;
+    }  
+    /**
+     * 生成宽比高5:4 的小于128kb的图片
+     *
+     * @param source 原图
+     */
+    public static Bitmap generateSpecialBitmap(Bitmap source) {
+        int width = source.getWidth();
+        int height = source.getHeight();
+        //128kb
+        long kb_128 = 1024 * 128;
+        Bitmap.Config config = source.getConfig();
+        long sample;
+        //计算图片类型 得出每像素占用空间
+        if (config == Bitmap.Config.ARGB_8888) {
+            sample = 4;
+        } else if (config == Bitmap.Config.ARGB_4444) {
+            sample = 2;
+        } else if (config == Bitmap.Config.ALPHA_8) {
+            sample = 1;
+        } else if (config == Bitmap.Config.RGB_565) {
+            sample = 2;
+        } else {
+            sample = 4;
+        }
+        kb_128 = kb_128 / sample;
+        long bitMapSize = width * height * sample;
+        if (kb_128 < bitMapSize) {
+            //图片大于128kb 需要缩小
+            float scale = 1.0f - bitMapSize / (kb_128 * 1.0f);
+            //计算需要缩小的scale
+            Matrix matrix = new Matrix();
+            matrix.postScale(scale, scale);
+            source = Bitmap.createBitmap(source, 0, 0, width, height, matrix, false);
+            width = source.getWidth();
+            height = source.getHeight();
+        }
+        //保持宽高比为5:4 居中截取
+        int newWidth;
+        int newHeight;
+        int deltaWidth;
+        int deltaHeight;
+        if (width <= height) {
+            //宽小于高 高变小 宽不变
+            newHeight = width * 4 / 5;
+            newWidth = width;
+            deltaWidth = 0; //width 变化的距离
+            deltaHeight = (height - newHeight) / 2;//y
+        } else {
+            //宽大于高 让宽为高度的 5 / 4  高度不变
+            newWidth = height * 5 / 4;
+            newHeight = height;
+            deltaWidth = (width - newWidth) / 2;
+            deltaHeight = 0;
+        }
+        Matrix m = new Matrix();
+        return Bitmap.createBitmap(source
+                , deltaWidth
+                , deltaHeight
+                , newWidth + deltaWidth > width ? width - deltaWidth : newWidth
+                , newHeight + deltaHeight > height ? height - deltaHeight : newHeight
+                , m, false);
     }
 }
